e92787ffead2b20fa6736bfae1e1d196c2f56853
Daniel Carvalho <daniel.devop@gmail.com>
2022-07-30 14:07:43 -0300

    teste de busca de usuario por nome

--- src/test/java/dev/unscrud/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/dev/unscrud/leilao/dao/UsuarioDaoTest.java
@@ -7,18 +7,28 @@ import javax.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import dev.unscrud.leilao.model.Usuario;
+import dev.unscrud.util.JPAUtil;
+
 public class UsuarioDaoTest {
 
   private UsuarioDao dao;
+  private Usuario usuario;
   private EntityManager em;
 
   @BeforeEach
   private void inicializar() {
+    em = JPAUtil.getEntityManager();
     this.dao = new UsuarioDao(em);
+    usuario = new Usuario("fulano", "fulano@unscrud.dev", "pass");
+    em.getTransaction().begin();
+    em.persist(usuario);
+    em.getTransaction().commit();
   }
 
   @Test
   void testeBuscarUsuarioPorUsername() {
-    assertNotNull(this.dao.buscarPorUsername("fulano"));
+    Usuario encontrado = this.dao.buscarPorUsername(usuario.getNome());
+    assertNotNull(encontrado);
   }
 }
