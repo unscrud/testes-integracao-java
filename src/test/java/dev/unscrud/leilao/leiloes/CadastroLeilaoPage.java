eb8ba0ad6d9c201a6493c39d1389e7ad095c6298
Daniel Carvalho <daniel.devop@gmail.com>
2022-08-05 21:24:54 -0300

    PageObject do cadastro de um leilão

---
+++ src/test/java/dev/unscrud/leilao/leiloes/CadastroLeilaoPage.java
@@ -0,0 +1,25 @@
+package dev.unscrud.leilao.leiloes;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CadastroLeilaoPage {
+  private WebDriver browser;
+
+  public CadastroLeilaoPage(WebDriver browser) {
+    this.browser = browser;
+  }
+
+  public void fechar() {
+    browser.quit();
+  }
+
+  public LeiloesPage cadastrarLeilao(String nome, String valorInicial, String dataAbertura) {
+    browser.findElement(By.id("nome")).sendKeys(nome);
+    browser.findElement(By.id("valorInicial")).sendKeys(valorInicial);
+    browser.findElement(By.id("dataAbertura")).sendKeys(dataAbertura);
+    browser.findElement(By.id("button-submit")).submit();
+    return new LeiloesPage(browser);
+  }
+
+}
