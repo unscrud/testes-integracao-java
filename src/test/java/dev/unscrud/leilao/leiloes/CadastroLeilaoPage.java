737f722831ce103d37064f21e695d9e0da1b5125
Daniel Carvalho <daniel.devop@gmail.com>
2022-08-05 22:29:07 -0300

    url testada e métodos de validacao

--- src/test/java/dev/unscrud/leilao/leiloes/CadastroLeilaoPage.java
+++ src/test/java/dev/unscrud/leilao/leiloes/CadastroLeilaoPage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CadastroLeilaoPage {
+  private final String BASE_URL = "http://localhost:8080/leiloes";
+  private final String NOVO_LEILAO_URL = BASE_URL + "/new";
+
   private WebDriver browser;
 
   public CadastroLeilaoPage(WebDriver browser) {
@@ -20,6 +23,17 @@ public class CadastroLeilaoPage {
     browser.findElement(By.id("dataAbertura")).sendKeys(dataAbertura);
     browser.findElement(By.id("button-submit")).submit();
     return new LeiloesPage(browser);
+  }
+
+  public boolean isPaginaAtual() {
+    return browser.getCurrentUrl().equals(NOVO_LEILAO_URL);
+  }
+
+  public boolean isMensagensDeValidacaoVisiveis() {
+    String pageSource = browser.getPageSource();
+    return pageSource.contains("minimo 3 caracteres")
+        && pageSource.contains("deve ser um valor maior de 0.1")
+        && pageSource.contains("deve ser uma data no formato dd/MM/yyyy");
   }
 
 }
